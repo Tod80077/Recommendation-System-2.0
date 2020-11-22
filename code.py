85f6f98345f4279c28842c23b2a07aa1340ba561
Omkar Udawant <oudawan1@binghamton.edu>
2020-11-22 18:52:47 -0500

    Updated files 3.0

--- code.py
+++ code.py
@@ -5,6 +5,7 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter import messagebox
 import numpy as np
 from error import rmse
 from sklearn.model_selection import train_test_split
@@ -21,12 +22,16 @@ class Root(Tk):
         self.configure(background = "#A9A9A9")
         self.labelFrame = ttk.LabelFrame(self, text = "------Recommenders System------")
         self.labelFrame.grid(column = 0, row = 1, padx = 20, pady = 20)
+        self.textarea = tk.Text(self)
+        self.textarea.grid(column = 0, row = 8)
         global n_flag
         self.n_flag = False
         self.button_find_file()
+        self.labelFrame = ttk.LabelFrame(self, text = "****** Algorithms ******")
+        self.labelFrame.grid(column = 0, row = 3, padx = 20, pady = 20)
         self.button_LinearRegression()
         self.button_RandomForestRegressor()
-        self.button_KNNClustering()
+        self.button_GenerateOutput()
         self.button_exit()
 
     def button_find_file(self):
@@ -35,19 +40,19 @@ class Root(Tk):
 
     def button_LinearRegression(self):
         self.button = ttk.Button(self.labelFrame, text = "Linear Regression", command = self.LinearRegression)
-        self.button.grid(column = 3, row = 1)
+        self.button.grid(column = 0, row = 3)
 
     def button_RandomForestRegressor(self):
         self.button = ttk.Button(self.labelFrame, text = "Random Forest Regressor", command = self.RandomForestRegressor)
-        self.button.grid(column = 3, row = 2)
+        self.button.grid(column = 0, row = 4)
 
-    def button_KNNClustering(self):
-        self.button = ttk.Button(self.labelFrame, text = "Generate Output file", command = self.KNNClustering)
-        self.button.grid(column = 3, row = 3)
+    def button_GenerateOutput(self):
+        self.button = ttk.Button(self.labelFrame, text = "Generate Output file using Random Forest Regressor", command = self.GenerateOutput)
+        self.button.grid(column = 0, row = 5)
 
     def button_exit(self):
         self.button = ttk.Button(self.labelFrame, text = "Exit", command = self.close_window)
-        self.button.grid(column = 1, row = 4)
+        self.button.grid(column = 0, row = 7)
 
     def close_window(self):
         self.destroy()
@@ -58,10 +63,13 @@ class Root(Tk):
         self.label = ttk.Label(self.labelFrame, text = "")
         self.label.grid(column = 1, row = 2)
         self.import_content()
+        self.DataSplit()
         if self.flag == True :
-            ttk.Label(text = "CSV File imported successfully!").grid(column = 0,row = 5)
+            self.textarea.delete("1.0","end")
+            self.textarea.insert(END, "CSV File imported successfully!")
         else:
-            ttk.Label(text = "Some error occured!").grid(column = 0,row = 5)
+            self.textarea.delete("1.0","end")
+            self.textarea.insert(END, "Some error occured!")
 
     def import_content(self):
         global data
@@ -75,7 +83,6 @@ class Root(Tk):
         users = 943
         items = 1682
         col = 3
-
         matrix_initialisation = np.zeros(users * items, col)     #Matrix initialisation with 0's
 
 
@@ -106,78 +113,95 @@ class Root(Tk):
         self.n_flag = True
 
     def LinearRegression(self):
-        self.label = ttk.Label(self.labelFrame, text = "")
-        self.label.grid(column = 1, row = 2)
         if self.n_flag == True :
             '''1. LinearRegression Model'''
-            print("**************** LinearRegression Model ****************")
-            self.DataSplit()
+            self.textarea.delete("1.0","end")
+            self.textarea.insert(END, "**************** LinearRegression Model ****************\n")
+
             reg = LinearRegression()
             reg.fit(train_x, train_y)
             predicted_L = reg.predict(test_x)
-            print('Model Score: ',reg.score(test_x, test_y))
+            text = 'Model Score: ' + str(reg.score(test_x, test_y))
+            self.textarea.insert(END, text + "\n")
             '''RMSE Value for Predicted'''
-            print('RMSE value for LinearRegression Model : ', rmse(predicted_L, test_y))
+            text = 'RMSE value for LinearRegression Model : ' + str(rmse(predicted_L, test_y))
+            self.textarea.insert(END, text + "\n")
         else:
-            ttk.Label(text = "Import CSV file first!").grid(column = 0,row = 5)
+            self.textarea.delete("1.0","end")
+            self.textarea.insert(END, "Import CSV file first!")
 
     def RandomForestRegressor(self):
-        self.label = ttk.Label(self.labelFrame, text = "")
-        self.label.grid(column = 1, row = 2)
         if self.n_flag == True :
             '''2. Random Forest Model'''
-            print("**************** Random Forest Model ****************")
-            self.DataSplit()
+
+            self.MsgBox = tk.messagebox.askyesno ('Clear TextArea','Do you want to clear TextArea?\n')
+            if self.MsgBox == True:
+                self.textarea.delete("1.0","end")
+                self.textarea.insert(END, "**************** Random Forest Model ****************\n")
+            else:
+                self.textarea.insert(END, "**************** Random Forest Model ****************\n")
+
             regr_R = RandomForestRegressor(max_depth=2, random_state=0)
             regr_R.fit(train_x, train_y)
 
             predicted_R = regr_R.predict(test_x)
-            print('Model Score: ',regr_R.score(test_x, test_y))
+            text = 'Model Score: ' + str(regr_R.score(test_x, test_y))
+            self.textarea.insert(END, text + "\n")
 
             '''RMSE Value for Predicted'''
-            print('RMSE value for Random Forest Model : ', rmse(predicted_R, test_y))
+            text = 'RMSE value for Random Forest Model : ' + str(rmse(predicted_R, test_y))
+            self.textarea.insert(END, text + "\n")
         else:
-            ttk.Label(text = "Import CSV file first!").grid(column = 0,row = 5)
+            self.textarea.delete("1.0","end")
+            self.textarea.insert(END, "Import CSV file first!\n")
 
+    def rmse_cal(self):
+        self.DataSplit()
+        regr_R = RandomForestRegressor(max_depth=2, random_state=0)
+        regr_R.fit(train_x, train_y)
 
-    def KNNClustering(self):
-        '''4. KNN Clustering Classifier '''
+        predicted_R = regr_R.predict(test_x)
+        text = 'Model Score: ' + str(regr_R.score(test_x, test_y))
+        self.textarea.insert(END, text + "\n")
+
+        '''RMSE Value for Predicted'''
+        text = 'RMSE value for Random Forest Model : ' + str(rmse(predicted_R, test_y))
+        self.textarea.insert(END, text + "\n")
+
+    def GenerateOutput(self):
         self.DataSplit2()
-        print("\n\n")
-        print("**************** KNN Model ****************")
+        self.textarea.insert(END, "\n\n**************** Random Forest Model ****************\n")
         neigh = KNeighborsClassifier()
         neigh.fit(train_x, train_y)
 
         predicted_K = neigh.predict(test_x)
-        print('Model Score: ',neigh.score(test_x, test_y))
+        text = 'Model Score: ' + str(neigh.score(test_x, test_y))
+        self.textarea.insert(END, text + "\n")
 
-        '''RMSE Value for Predicted'''
-        print('RMSE value for Random Forest Model : ', rmse(predicted_K, test_y))
-        print("\n\n")
+        self.rmse_cal()
 
-        users =  15
-        items = 10
-        ratings = 0#1682
+        self.DataSplit2()
+
+        users =  943 #1...943
+        items = 1682 #1...1682
+        ratings = 0  #1...5
 
         file_output = open("Predicted_values.txt", "w+")
 
-        print("\n")
-        print("Predicting the ratings given for 15 items by 10 users  ....")
+        self.textarea.insert(END, "Predicting the ratings given for 943 users and 1682 items ....")
+        self.textarea.insert(END, "\n\n\t\tWait time 12 mins or less...\n")
+        self.MsgBox = tk.messagebox.showinfo("Generated Output File", "Predicted the ratings given for 943 users and 1682 items ....\nOUTPUT File name: Predicted_values.txt")
+
         Predicted_Ratings = []
 
         for i in range(1,users+1):
             for j in range(1,items+1):
                 rating = neigh.predict([[i,j]])
-                #Actual_rating = ( rating * (max_rating - min_rating)) + min_rating
-                #Predicted_Ratings.append(rating)
-                #print(i, j, rating)
                 val = str(i)+" "+str(j)+" "+str(rating[0]) + "\n"
                 file_output.write(val)
-            #print( )
 
-        print("Done!")
+        self.MsgBox = tk.messagebox.showinfo("Done", "File Generated with name Predicted_values.txt!")
         file_output.close()
-       # print("Predicted Ratings are : ", Predicted_Ratings)
 
 if __name__ == '__main__':
     root = Root()
